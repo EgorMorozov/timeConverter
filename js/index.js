07b9c70722d5df6ad5ce270f2c4032d865cdba9a
Victor <me@bjatta.ru>
2018-04-19 18:31:10 +0300

    zero fix

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@
             $.querySelector('input#time').addEventListener('input', event => {
                     let value = event.target.value;
                     if (value > 60) event.target.value = 60;
-                    if (value < 1) event.target.value = 1;
+                    if (value < 0) event.target.value = 0;
                     $.querySelector('div#result').innerHTML = (event.target.value / 60).toFixed(2);
                 }
             )
